278f4928047870f9ff42dbfd79550ecb3f3fa365
akavity <basilakavity@gmail.com>
2023-02-28 13:31:53 +0300

    feat: add Consumer

---
+++ src/main/java/home_work_1/produser_consumer/job/Consumer.java
@@ -0,0 +1,34 @@
+package home_work_1.produser_consumer.job;
+
+import home_work_1.produser_consumer.utils.FileUtil;
+
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+
+public class Consumer implements Runnable {
+    private final Queue<Integer> queue;
+
+    public Consumer(Queue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        FileUtil file = new FileUtil("test.txt");
+        Integer sleepTime = null;
+        while ((sleepTime = queue.poll()) != null) {
+            sleep(sleepTime);
+            file.write(" - I slept " + sleepTime + " second(s)");
+        }
+        file.write(" ...");
+        sleep(1);
+    }
+
+    private void sleep(Integer time) {
+        try {
+            TimeUnit.SECONDS.sleep(time);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
