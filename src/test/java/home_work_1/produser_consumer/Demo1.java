979777a95da406af6dc20edd61456faa5109325f
akavity <basilakavity@gmail.com>
2023-02-28 13:33:58 +0300

    feat: add Demo

---
+++ src/test/java/home_work_1/produser_consumer/Demo1.java
@@ -0,0 +1,24 @@
+package home_work_1.produser_consumer;
+
+import home_work_1.produser_consumer.job.Consumer;
+import home_work_1.produser_consumer.supplier.Producer;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Demo1 {
+    public static void main(String[] args) throws InterruptedException {
+        Queue<Integer> queueContainer = new LinkedBlockingQueue<>();
+
+        Thread producer = new Thread(new Producer(queueContainer));
+        Thread consumer1 = new Thread(new Consumer(queueContainer));
+        Thread consumer2 = new Thread(new Consumer(queueContainer));
+        Thread consumer3 = new Thread(new Consumer(queueContainer));
+
+        producer.start();
+        producer.join();
+        consumer1.start();
+        consumer2.start();
+        consumer3.start();
+    }
+}
