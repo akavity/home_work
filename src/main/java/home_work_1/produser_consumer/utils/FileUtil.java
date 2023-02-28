dd24234ed617d8b204305f168647dfde244d5778
akavity <basilakavity@gmail.com>
2023-02-28 13:32:54 +0300

    feat: add FileUtil

---
+++ src/main/java/home_work_1/produser_consumer/utils/FileUtil.java
@@ -0,0 +1,24 @@
+package home_work_1.produser_consumer.utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FileUtil {
+    private final File file;
+
+    public FileUtil(String fileName) {
+        this.file = new File(fileName);
+    }
+
+    public void write(String text) {
+        try(FileWriter writer = new FileWriter(file, true);) {
+            writer.write(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"))
+                    + "-" + Thread.currentThread().getName() + text + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
