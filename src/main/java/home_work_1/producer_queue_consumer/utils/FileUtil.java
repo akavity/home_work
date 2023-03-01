03494f88434db590a6f844cc87a59a928ec747f5
akavity <basilakavity@gmail.com>
2023-03-02 00:58:25 +0300

    fix: deleted producer.join

--- src/main/java/home_work_1/producer_queue_consumer/utils/FileUtil.java
+++ src/main/java/home_work_1/producer_queue_consumer/utils/FileUtil.java
@@ -1,13 +1,18 @@
 package home_work_1.producer_queue_consumer.utils;
 
+import home_work_1.producer_queue_consumer.supplier.Producer;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileUtil {
     private final File file;
+    public static final Logger logger = Logger.getLogger(Producer.class.getName());
     private final String localDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
 
     public FileUtil(String fileName) {
@@ -15,10 +20,10 @@ public class FileUtil {
     }
 
     public void write(String text) {
-        try (FileWriter writer = new FileWriter(file, true);) {
+        try (FileWriter writer = new FileWriter(file, true)) {
             writer.write(localDateTime + "-" + Thread.currentThread().getName() + text + "\n");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Error message", e);
         }
     }
 }
