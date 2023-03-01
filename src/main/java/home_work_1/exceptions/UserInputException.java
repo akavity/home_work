62d4281ecee5613695128c202e3d156751b90ef4
akavity <basilakavity@gmail.com>
2023-03-01 22:03:08 +0300

    add constructors

--- src/main/java/home_work_1/exceptions/UserInputException.java
+++ src/main/java/home_work_1/exceptions/UserInputException.java
@@ -1,7 +1,19 @@
 package home_work_1.exceptions;
 
-public class UserInputException extends RuntimeException {
+public class UserInputException extends IllegalArgumentException {
+    public UserInputException() {
+        super("Wrong data");
+    }
+
     public UserInputException(String massage) {
         super(massage);
+    }
+
+    public UserInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserInputException(Throwable cause) {
+        super(cause);
     }
 }
