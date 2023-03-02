d69a849036c1b17f5c182815a9d2421562198eef
akavity <basilakavity@gmail.com>
2023-03-02 19:38:43 +0300

    feat: add Task02

---
+++ src/main/java/test/task02/Task02.java
@@ -0,0 +1,24 @@
+package test.task02;
+
+public class Task02 {
+    public static void main(String[] args) {
+        Shop santaShop = new Shop();
+
+        Product bread = new Product("Bread", 12.5);
+        Product milk = new Product("Milk", 10.5);
+        Product cucumber = new Product("Cucumber", 18);
+        Product sausages = new Product("Sausages", 18);
+
+        santaShop.addProductsInShop(bread, 40);
+        santaShop.addProductsInShop(milk, 80);
+        santaShop.addProductsInShop(cucumber, 100);
+        santaShop.addProductsInShop(sausages, 50);
+
+        Basket myBasket = santaShop.getBasket();
+
+
+
+
+    }
+
+}
